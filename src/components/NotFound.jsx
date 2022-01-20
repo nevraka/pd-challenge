c4f6bd52e358cc2fc442104da6111db8bff13544
nevraka <nevrakaya@gmail.com>
2022-01-20 09:49:23 +0100

    not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
+import notfound from './notfound.png';
 
 const NotFound = () => {
   return (
-    <>
-      <div className="flex flex-col items-center mt-24">
-        <div>
-          <h1 className="text-2xl font-medium"> Page not found!</h1>
-        </div>
-        <img src="/image/notfound.png" alt="404" className="w-96 pt-24"></img>
-        <Link to={'/'}>
-          <div className="py-8 text-l font-medium underline">Home Page</div>
-        </Link>
+    <div className="flex flex-col items-center mt-24">
+      <div>
+        <h1 className="text-2xl font-medium">Page not found!</h1>
       </div>
-    </>
+      {/* TODO import image directly */}
+      <img src={notfound} alt="404" className="w-96 pt-24"></img>
+      <Link to={'/'}>
+        <div className="py-8 text-l font-medium underline">Home Page</div>
+      </Link>
+    </div>
   );
 };
 
