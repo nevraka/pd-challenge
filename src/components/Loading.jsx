a945d898a651839274ffd89b469139ed1bde6c0f
nevraka <nevrakaya@gmail.com>
2022-01-20 09:49:13 +0100

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,7 @@
 const Loading = () => {
   return (
-    <div>
-      <div
-        className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-purple-500"
-        role="status"
-      >
-        <span className="visually-hidden">Loading...</span>
-      </div>
+    <div className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0 text-purple-500">
+      <span className="visually-hidden">Loading...</span>
     </div>
   );
 };
